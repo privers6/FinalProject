bc6c5af978f43e4c24beffea98838d5d580d9499
Andrew Smith <asmith379@gatech.edu>
2015-02-10 17:01:19 -0500

    Changed background to a solid hex color, and added javadocs.

--- app/src/main/java/com/example/twig/finalproject/User.java
+++ app/src/main/java/com/example/twig/finalproject/User.java
@@ -1,25 +1,47 @@
 package com.example.twig.finalproject;
 
 /**
- * Created by Andrew on 2/5/2015.
+ * Class containing the information for a user.
+ *
+ * @author Andrew
  */
 public class User {
     private String name;
     private String password;
 
+    /**
+     * Constructor for a User object.
+     *
+     * @param n - the user's username
+     * @param p - the user's password
+     */
     public User(String n, String p) {
         name = n;
         password = p;
     }
 
+    /**
+     * Getter for the username.
+     * @return the username
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for the user's password.
+     *
+     * @return the password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter to change the user's password.
+     *
+     * @param newPass - the new password
+     */
     public void setPassword(String newPass) {
         password = newPass;
     }
